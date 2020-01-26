7dc59f41c153cdf94a58c5b8ac8f12296237d327
KOREApsh <tmdgks22222@gmail.com>
2020-01-26 21:30:20 +0900

    Update MainActivity.java
    [add]test gitdesktop

--- BarcodeWidgetF102/app/src/main/java/com/f10company/barcodewidgetf10/MainActivity.java
+++ BarcodeWidgetF102/app/src/main/java/com/f10company/barcodewidgetf10/MainActivity.java
@@ -49,7 +49,7 @@ import static com.google.zxing.integration.android.IntentIntegrator.CODE_128;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    //EsterEgg mother Ffffff
+    //EsterEgg mother Ffffff//test
     SharedPreferences prefs;
     private ImageButton scanQRBtn;
     TextView tv;
